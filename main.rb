2c378fe6482745d76c1c7ba1db55dd7f0476af5c
fenroe <scrambles9494@gmail.com>
2023-07-17 21:12:30 +0300

    Added the logic for the program
    Program should now work according to requirements

--- main.rb
+++ main.rb
@@ -9,17 +9,36 @@ cipher_key = gets.chomp().to_i
 # TODO:
 # encode the message
 # split message_to_encode into an array
+message_array = message_to_encode.split(//)
 # create an array of letters from a-z
+alphabet = [*"a"..."z"]
 # create an empty array for the encoded message
+encoded_message_array = []
 # loop through the message array
-# mark whether a letter is uppercase
-# convert letter to lowercase so we don't have to make two alphabet arrays
-# if character isn't in the alphabet array (whitespace, comma, etc) then just add the character to the encoded array
-# else, add the index of the character in the alphabet array to the cipher key number
-# adjust this sum so that it's between 0 and 25
-# return the letter in the alphabet array with the same index as this sum
+for character in message_array
+    # mark whether a letter is uppercase
+    character_is_uppercase = character == character.upcase
+    # convert letter to lowercase so we don't have to make two alphabet arrays
+    character.downcase
+    # if character isn't in the alphabet array (whitespace, comma, etc) then just add the character to the encoded array
+    if alphabet.include?(character) == false
+        encoded_message_array.push(character)
+    # else, add the index of the character in the alphabet array to the cipher key number
+    else
+        new_character_index = cipher_key + alphabet.find_index(character)
+        # adjust this sum so that it's between 0 and 25
+        while new_character_index > 25 || new_character_index < 0
+            new_character_index -= 26 if new_character_index > 25
+            new_character_index += 26 if new_character_index < 0
+        end
+        # return the letter in the alphabet array with the same index as this sum
+        new_character = alphabet[new_character_index]
+        # add this new character to the encoded message array
+        encoded_message_array.push(new_character)
+    end
+end
 # join the encoded message array into a string
+encoded_message = encoded_message_array.join
 # return the encoded message
-encoded_message = "This is an encoded message"
 puts("Here is your encoded message:")
 puts(encoded_message)
